565947fc6f93f053fe8db5bdafb6d917fc8b27b6
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-07-30 11:10:20 +0200

    update travis build

---
+++ prepare-travis.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+# Prepare the environment on travis
+
+# Copyright (C) 2020 Free Software Foundation, Inc.
+# Released under GNU General Public License, either version 3
+# or any later option
+
+# Uninstall Homebrew
+brew --version
+/usr/bin/sudo /usr/bin/find /usr/local -mindepth 2 -delete && hash -r
+
+# Guard against intermittent Travis CI DNS outages
+for host in distfiles.macports.org dl.bintray.com github.com packages.macports.org packages-private.macports.org rsync-origin.macports.org; do
+    dig +short "$host" | sed -n '$s/$/ '"$host/p" | sudo tee -a /etc/hosts >/dev/null
+done
