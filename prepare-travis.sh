861447c4d92fdc0a0b823fba09ccf7b6f44b60a6
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-07-31 12:56:54 +0200

    update build

--- prepare-travis.sh
+++ prepare-travis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/sh -xve
 
 # Prepare the environment on travis
 
@@ -11,6 +11,12 @@ brew --version
 /usr/bin/sudo /usr/bin/find /usr/local -mindepth 2 -delete && hash -r
 
 # Guard against intermittent Travis CI DNS outages
-for host in distfiles.macports.org dl.bintray.com github.com packages.macports.org packages-private.macports.org rsync-origin.macports.org; do
+for host in www.hs-augsburg.de distfiles.macports.org dl.bintray.com github.com packages.macports.org packages-private.macports.org rsync-origin.macports.org; do
     dig +short "$host" | sed -n '$s/$/ '"$host/p" | sudo tee -a /etc/hosts >/dev/null
 done
+
+# Download and install the macports build environment for pspp
+pushd /opt/macports
+curl -o macports.tgz https://www.hs-augsburg.de/homes/beckmanf/pspp/macports-pspp.tgz
+tar -xvzf macports.tgz
+popd
