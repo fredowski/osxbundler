cf3f2068c48ac1b1cafb8d2243e219b6b95c5894
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-07-31 19:40:22 +0200

    remove download from prepare travis

--- prepare-travis.sh
+++ prepare-travis.sh
@@ -14,9 +14,3 @@ brew --version
 for host in www.hs-augsburg.de distfiles.macports.org dl.bintray.com github.com packages.macports.org packages-private.macports.org rsync-origin.macports.org; do
     dig +short "$host" | sed -n '$s/$/ '"$host/p" | sudo tee -a /etc/hosts >/dev/null
 done
-
-# Download and install the macports build environment for pspp
-pushd /opt/macports
-curl -o macports.tgz https://www.hs-augsburg.de/homes/beckmanf/pspp/macports-pspp.tgz
-tar -xzf macports.tgz
-popd
