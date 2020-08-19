28ff682ac0da2ea87e16f9050a42f68becfc4494
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2020-08-19 15:12:50 +0200

    download macports-ports repo

--- install-build-env.sh
+++ install-build-env.sh
@@ -8,4 +8,8 @@
 pushd /opt/macports
 curl -o macports.tgz https://www.hs-augsburg.de/homes/beckmanf/pspp/macports-pspp.tgz
 tar -xzf macports.tgz
+git clone --depth=5 --no-single-branch https://github.com/fredowski/macports-ports.git
+cd macports-ports
+git checkout pspp/1.3.0-1
+../install/bin/portindex
 popd
