84b615ccabe6fb7759cbaf36721a28448eac3d1d
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2022-03-18 09:50:50 +0100

    adapted makeicns to new icon naming convention

--- buildpspp.sh
+++ buildpspp.sh
@@ -130,10 +130,12 @@ case $buildsource in
   "--brew")
       psppversion=`brew info pspp | sed -n 's/.* stable \([0-9]\.[0-9]\.[0-9]\).*/\1/p'`
       fullreleaseversion=$psppversion-$bundleversion
+      psppiconname=pspp
       brew install --verbose --with-relocation pspp;;
   "--brew-nightly")
       echo "Build nightly brew HEAD"
       brew install --verbose --with-relocation --HEAD pspp
+      psppiconname=org.fsf.pspp
       psppversion=`pspp --version | sed -n 's/pspp (GNU PSPP) \(.*\)/\1/p'`
       fullreleaseversion=$psppversion-$bundleversion;;
   *)
@@ -149,9 +151,9 @@ else
 fi
 
 # Create the icns file
-makeicns -256 $psppiconpath/share/icons/hicolor/256x256/apps/pspp.png \
-         -32  $psppiconpath/share/icons/hicolor/32x32/apps/pspp.png \
-         -16  $psppiconpath/share/icons/hicolor/16x16/apps/pspp.png \
+makeicns -256 $psppiconpath/share/icons/hicolor/256x256/apps/$psppiconname.png \
+         -32  $psppiconpath/share/icons/hicolor/32x32/apps/$psppiconname.png \
+         -16  $psppiconpath/share/icons/hicolor/16x16/apps/$psppiconname.png \
          -out pspp.icns
 # Set version information
 sed "s/0.10.1/$fullreleaseversion/g" Info-pspp.plist > Info-pspp-version.plist
