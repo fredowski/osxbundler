68b544ebd6e62a5ee1c9e5295e279c7ae64fd2ca
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2022-06-20 11:29:17 +0200

    application id change from org.fsf.pspp to org.gnu.pspp

--- buildpspp.sh
+++ buildpspp.sh
@@ -130,11 +130,13 @@ case $buildsource in
   "--brew")
       psppversion=`brew info pspp | sed -n 's/.* stable \([0-9]\.[0-9]\.[0-9]\).*/\1/p'`
       fullreleaseversion=$psppversion-$bundleversion
+      pspporgname="org.fsf"
       brew install --verbose --with-relocation pspp;;
   "--brew-nightly")
       echo "Build nightly brew HEAD"
       brew install --verbose --with-relocation --HEAD pspp
       psppversion=`pspp --version | sed -n 's/pspp (GNU PSPP) \(.*\)/\1/p'`
+      pspporgname="org.gnu"
       fullreleaseversion=$psppversion-$bundleversion;;
   *)
       echo "Option $1 not valid. Exiting"
@@ -149,9 +151,9 @@ else
 fi
 
 # Create the icns file
-makeicns -256 $psppiconpath/share/icons/hicolor/256x256/apps/org.fsf.pspp.png \
-         -32  $psppiconpath/share/icons/hicolor/32x32/apps/org.fsf.pspp.png \
-         -16  $psppiconpath/share/icons/hicolor/16x16/apps/org.fsf.pspp.png \
+makeicns -256 $psppiconpath/share/icons/hicolor/256x256/apps/$pspporgname.pspp.png \
+         -32  $psppiconpath/share/icons/hicolor/32x32/apps/$pspporgname.pspp.png \
+         -16  $psppiconpath/share/icons/hicolor/16x16/apps/$pspporgname.pspp.png \
          -out pspp.icns
 # Set version information
 sed "s/0.10.1/$fullreleaseversion/g" Info-pspp.plist > Info-pspp-version.plist
